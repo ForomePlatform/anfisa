35b77d33552e4763ba5cdb3effd7df0ecab7c619
Sergey Trifonov <trf@yandex.com>
2018-09-12 01:15:56 +0300

    add condition maker

---
+++ app/search/condition.py
@@ -0,0 +1,16 @@
+#===============================================
+class ConditionMaker:
+    @staticmethod
+    def condNumGE(unit_name, bound, use_undef = False):
+        return ["numeric", unit_name, True, bound, use_undef]
+
+    @staticmethod
+    def condNumLE(unit_name, ge_mode, bound, use_undef = False):
+        return ["numeric", unit_name, False, bound, use_undef]
+
+    @staticmethod
+    def condEnum(unit_name, variants, join_mode = "AND"):
+        assert join_mode in {"AND", "OR", "ONLY", "NOT"}
+        return ["enum", unit_name, join_mode, variants]
+
+
