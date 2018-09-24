2cd24d03a9a21d89df02df7c84563f113fd7cfac
mmcentre <michael.bouzinier@kvitneset.com>
2018-09-24 16:55:46 -0400

    MAB001             Filtering and annotating variants

--- app/search/hot_eval/quality_check.py
+++ app/search/hot_eval/quality_check.py
@@ -1,9 +1,7 @@
 def evalRec(env, rec):
     """Quality check"""
 
-    if not rec.Proband_has_Variant:
-        return False
-    elif rec.Proband_GQ < env.gq:
+    if rec.Proband_GQ < env.gq:
         return False
     elif rec.FS > env.fs:
         return False
--- app/search/hot_eval/rare_check.py
+++ app/search/hot_eval/rare_check.py
@@ -2,20 +2,11 @@ def evalRec(env, rec):
     """Candidates (Rare)"""
 
     # standard part of evaluation
-    quality = True
-    if not rec.Proband_has_Variant:
-        quality = False
-    elif rec.Proband_GQ < env.gq:
-        quality = False
-    elif rec.FS > env.fs:
-        quality = False
-    elif rec.QD < env.qd:
-        quality = False
+    if ("Quality-PASS" not in rec.Rules):
+        return False
+
     known = len(rec.Presence_in_Databases & {"ClinVar", "HGMD"}) > 0
 
-    # "rare" variant of check evaluation
-    if (not quality):
-        return False
     if (known):
         min_frequency = env.af_in_db
     else:
