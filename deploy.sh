eb58075cbfd44adac9fc48744446430cf901fc1e
mmcentre <michael.bouzinier@kvitneset.com>
2018-10-16 20:37:03 -0400

    MAB001             Filtering and annotating variants

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 
 cd data
 wget https://www.dropbox.com/s/duj0r1ccgjj1olv/PGP3140.json
-cd ../export/work
+cd ..tmp/export
 wget https://www.dropbox.com/s/qvi229bfdtfxyrw/SEQaBOO_output_template_0730.xlsx
 cd ../..
 
