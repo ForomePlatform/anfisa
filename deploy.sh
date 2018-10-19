cc170c04696a2abc6b906fbfd45c21b4de669eba
mmcentre <michael.bouzinier@kvitneset.com>
2018-10-19 00:35:03 -0400

    MAB 0002 Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+repo=`pwd`
+target=$repo
+if [ -f .inst_dir ] ; then
+    target=`cat .inst_dir`
+fi
+
+read -p "Installation directory: ${target}? (y/n)" response
+if [ $response != "y" ] && [ $response != "Y" ] ; then
+    read -p "Type directory: " target
+fi
+
+read -p "Install Anfisa into ${target}? (y/n)" response
+if [ $response != "y" ] && [ $response != "Y" ] ; then
+    echo "Installation aborted"
+    return 1 2> /dev/null || exit 1
+fi
+
+if [ $repo != $target ] ; then
+    echo Setting up $target
+    mkdir -p $target
+    cp $repo/anfisa.json $target
+    echo $target > .inst_dir
+    cd $target
+else
+    echo "Installing into repository directory"
+fi
+
+
 [ ! -d "data" ] && mkdir data
 [ ! -d "tmp/export/work" ] && mkdir -p tmp/export/work
 [ ! -d "logs" ] && mkdir logs
