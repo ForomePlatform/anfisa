be5d615f93cefb0a1aa0e9c4f1fab1e7b3541c9c
Sergey Trifonov <trf@ya.ru>
2018-11-05 22:12:36 +0300

    export directory fix

--- deploy.sh
+++ deploy.sh
@@ -29,16 +29,16 @@ fi
 
 
 [ ! -d "data" ] && mkdir data
-[ ! -d "tmp/export/work" ] && mkdir -p tmp/export/work
+[ ! -d "export/work" ] && mkdir -p export/work
 [ ! -d "logs" ] && mkdir logs
 
 cd data
 rm *
 wget https://www.dropbox.com/s/duj0r1ccgjj1olv/PGP3140.json
-cd ../tmp/export
+cd ../export
 [ ! -f SEQaBOO_output_template_0730.xlsx ] && wget https://www.dropbox.com/s/qvi229bfdtfxyrw/SEQaBOO_output_template_0730.xlsx
 
-cd ../..
+cd ..
 echo "Updating configuration in anfisa.json"
 hostname=`hostname`
 sed  's#${HOME}/../a-setup#WOWOWOWO#' anfisa.json | sed "s#WOWOWOWO#$target#" > anfisa_$hostname.json
