8dc56396dfb4d1129071e9681b5601eabf4d0f9d
mmcentre <michael.bouzinier@kvitneset.com>
2018-11-06 13:45:39 -0500

    MAB006             Anfisa v.0.2 (Added reportable Hearing Loss gene List)

--- annotations/annotator.py
+++ annotations/annotator.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 
 from annotations import case_utils, liftover
 from annotations.clinvar import ClinVar
@@ -7,8 +8,41 @@ from annotations.gnomad import GnomAD
 from annotations.hgmd import HGMD
 from annotations.record import Variant
 
+VEP_HOME = "/db/vep-93/ensembl-vep/"
+DB_HOME = "/db/data"
+TMP = "/data/tmp"
 
-def process_file(f, out = None, vcf_header = None, samples = None, case = None, limit = None):
+sample_command = "{vep_home}/vep" \
+                 " --cache --merged --dir_cache {db_home}/vep/cache --dir {db_home}/vep/cache --port 3337 " \
+                 "--force_overwrite --everything --json -i {input} -o {output} " \
+                 "--plugin dbNSFP,{db_home}/dbNSFPa/dbNSFP_hg19.gz,Polyphen2_HDIV_pred,Polyphen2_HVAR_pred,Polyphen2_HDIV_score,Polyphen2_HVAR_score,SIFT_pred,SIFT_score,MutationTaster_pred,MutationTaster_score,FATHMM_pred,FATHMM_score,REVEL_score,CADD_phred,CADD_raw,MutationAssessor_score,MutationAssessor_pred,clinvar_rs,clinvar_clnsig " \
+                 "--plugin Conservation " \
+                 "--plugin ExACpLI,{db_home}/misc/ExACpLI_values.txt " \
+                 "--plugin MaxEntScan,{db_home}/MaxEntScan/fordownload " \
+                 "--plugin LoFtool,{db_home}/loftoll/LoFtool_scores.txt  " \
+                 "--plugin SpliceRegion " \
+                 "--plugin GeneSplicer,{db_home}/GeneSplicer/bin/linux/genesplicer,{db_home}/GeneSplicer/human,context=200,tmpdir={tmp} " \
+                 "--fasta {db_home}/vep/cache/homo_sapiens/93_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz " \
+                 "--fork {fork}"
+
+
+def execute_vep(input, output = None, fork = 8):
+    if (not output):
+        x = input.split('.')
+        if (x[-1].lower() == 'vcf'):
+            x = x[:-1]
+        x.append("vep")
+        x.append("json")
+        output = '.'.join(x)
+    t0 = time.time()
+    cmd_string = sample_command.format(vep_home=VEP_HOME, db_home=DB_HOME, input=input, output=output, tmp=TMP, fork=fork)
+    print cmd_string
+    os.system(cmd_string)
+    print "VEP has completed annotation. Duration: {} s.".format(time.time() - t0)
+    return output
+
+
+def annotate_json(f, out = None, vcf_header = None, samples = None, case = None, limit = None):
     n = 0
     hg19_to_38_converter = liftover.Converter()
 
@@ -44,10 +78,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Annotate VCF file with VEP and output results as JSON")
     parser.add_argument("-i", "--input", dest = "input", help="Input JSON file, with VEP annotations")
     parser.add_argument("-o", "--output", dest="output", help="Output file")
+    parser.add_argument("--vep", action='store_true', help="Annotate with VEP first")
     parser.add_argument("-c", "--case", dest="case", help="Case name, default is determined from directory name")
     parser.add_argument("-d", "--dir", dest="dir", help="Work directory", default=os.getcwd())
     parser.add_argument("-l", "--limit", dest="limit", type=int, help="Maximum number of variants top process")
     parser.add_argument("--header", help="VCF Header file", default="header.vcf")
+    parser.add_argument("--fork", help="Number of parallel processes", default=8, type=int)
 
     args = parser.parse_args()
     print args
@@ -58,7 +94,11 @@ if __name__ == '__main__':
     case = args.case if args.case else os.path.basename(dir).split('_')[0]
 
     fam_file = "{}.fam".format(case)
-    filtered_by_bed_vep_output = args.input if args.input else "{}_wgs_xbrowse.vep.filtered.vep.json".format(case)
+    if (args.vep):
+        filtered_by_bed_vep_input = args.input if args.input else "{}_wgs_xbrowse.vep.filtered.vcf".format(case)
+        filtered_by_bed_vep_output = execute_vep(filtered_by_bed_vep_input, None, args.fork)
+    else:
+        filtered_by_bed_vep_output = args.input if args.input else "{}_wgs_xbrowse.vep.filtered.vep.json".format(case)
     limit = args.limit
     print "limit = {}".format(limit)
     print "file: {}".format(filtered_by_bed_vep_output)
@@ -79,5 +119,5 @@ if __name__ == '__main__':
 
     output = args.output if args.output else "{}/{}_anfisa.json".format(dir, case)
 
-    process_file(filtered_by_bed_vep_output, out=output,
-                     vcf_header=header, samples=samples, case="{}_wgs".format(case), limit =limit)
+    annotate_json(filtered_by_bed_vep_output, out=output,
+                  vcf_header=header, samples=samples, case="{}_wgs".format(case), limit =limit)
--- annotations/record.py
+++ annotations/record.py
@@ -883,20 +883,16 @@ class Variant:
                 data[c] = v
         return data
 
-    def get_view_json(self):
-        data_info = self.data.copy()
-        view_info = dict()
-        filters   = self.filters.copy()
-
-        data_info['label'] = self.get_label()
-        data_info['color_code'] = self.get_color_code()
-
+    def create_general_tab(self, data_info, view_info, filters):
         tab1 = dict()
         view_info["general"] = tab1
         tab1['genes'] = self.get_genes()
         tab1['hg19'] = str(self)
         tab1['hg38'] = self.get_hg38_coordinates()
-        if (not self.is_snv()):
+        if (self.is_snv()):
+            data_info["ref"] = self.ref()
+            data_info["alt"] = self.alt_string()
+        else:
             tab1["ref"] = self.ref()
             tab1["alt"] = self.alt_string()
 
@@ -938,8 +934,23 @@ class Variant:
         tab1['variant_intron_worst'] = self.get_from_worst_transcript("intron")
         tab1['variant_exon_canonical'] = self.get_from_canonical_transcript("exon")
         tab1['variant_intron_canonical'] = self.get_from_canonical_transcript("intron")
+
+        intron_or_exon = tab1['variant_exon_canonical'] if tab1['variant_exon_canonical'] else tab1['variant_intron_canonical']
+        e = intron_or_exon.split('/')
+        data_info["variant_exon_intron_canonical"] = e[0]
+        if (len(e) > 1):
+            data_info["total_exon_intron_canonical"] = e[1]
+
+        intron_or_exon = tab1['variant_exon_worst'] if tab1['variant_exon_worst'] else tab1['variant_intron_worst']
+        e = intron_or_exon.split('/')
+        data_info["variant_exon_intron_worst"] = e[0]
+        if (len(e) > 1):
+            data_info["total_exon_intron_worst"] = e[1]
+
+
         tab1["igv"] = self.get_igv_url()
 
+    def create_quality_tab(self, data_info, view_info, filters):
         tab2 = list()
         view_info["quality_samples"] = tab2
         q_all = dict()
@@ -974,6 +985,7 @@ class Variant:
             q_s['genotype_quality'] = self.get_from_genotype(genotype, 'GQ')
             tab2.append(q_s)
 
+    def create_gnomad_tab(self, data_info, view_info, filters):
         tab3 = list()
         view_info["gnomAD"] = tab3
         if (self.get_gnomad_af()):
@@ -1000,6 +1012,7 @@ class Variant:
                 gr["url"] = \
                     "http://gnomad.broadinstitute.org/variant/{}-{}-{}-{}".format(self.chr_num(), self.start(), self.ref(), allele)
 
+    def create_databases_tab(self, data_info, view_info, filters):
         tab4 = dict()
         view_info["databases"] = tab4
         genes = self.get_genes()
@@ -1010,6 +1023,7 @@ class Variant:
 
         if (omim_ids):
             omim_urls = ["https://www.omim.org/entry/{}".format(omim_id) for omim_id in omim_ids]
+            data_info["omim_ids"] = omim_ids
         else:
             omim_urls = [
                 "https://omim.org/search/?search=approved_gene_symbol:{}&retrieve=geneMap".format(gene) for gene in genes
@@ -1024,6 +1038,7 @@ class Variant:
         pmids = self.private_data.get("HGMD_PIMIDs")
         tab4["hgmd_tags"] = self.private_data.get("HGMD_TAGs")
         tab4["hgmd_pmids"] = [link_to_pmid(pmid[1]) for pmid in pmids] if pmids else None
+        data_info["hgmd_pmids"] = [pmid[1]for pmid in pmids] if pmids else None
         phenotypes = self.private_data.get("HGMD_phenotypes")
         tab4["hgmd_phenotypes"] = [p[0] for p in phenotypes] if phenotypes else None
 
@@ -1036,6 +1051,7 @@ class Variant:
 
         tab4["pubmed_search"] = self.get_tenwise_link()
 
+    def create_predictions_tab(self, data_info, view_info, filters):
         tab5 = dict()
         #view['Predictions'] = tab5
         view_info["predictions"] = tab5
@@ -1062,6 +1078,7 @@ class Variant:
         tab5["cadd_raw"] = unique(self.get_from_transcripts_list("cadd_raw"))
         tab5["mutation_assessor"] = unique(self.get_from_transcripts_list("mutationassessor_pred"))
 
+    def create_bioinformatics_tab(self, data_info, view_info, filters):
         tab6 = dict()
         view_info["bioinformatics"] = tab6
         tab6["zygosity"] = self.get_zygosity()
@@ -1077,6 +1094,22 @@ class Variant:
         tab6["other_genes"] = self.get_other_genes()
         tab6['called_by'] = self.get_callers()
         tab6['caller_data'] = self.get_callers_data()
+
+
+    def get_view_json(self):
+        data_info = self.data.copy()
+        view_info = dict()
+        filters   = self.filters.copy()
+
+        data_info['label'] = self.get_label()
+        data_info['color_code'] = self.get_color_code()
+        self.create_general_tab(data_info, view_info, filters)
+        self.create_quality_tab(data_info, view_info, filters)
+        self.create_gnomad_tab(data_info, view_info, filters)
+        self.create_databases_tab(data_info, view_info, filters)
+        self.create_predictions_tab(data_info, view_info, filters)
+        self.create_bioinformatics_tab(data_info, view_info, filters)
+
 
         tab7 = dict()
         view_info["inheritance"] = tab7
