dd4c6a3bbd928d6e15f0e3c8e72fc0ff41ad2dbf
mmcentre <michael.bouzinier@kvitneset.com>
2019-01-05 21:17:28 -0500

    MAB008         Anfisa version 2.0

--- annotations/utils/gtf_import.py
+++ annotations/utils/gtf_import.py
@@ -43,68 +43,75 @@ class GTF_Import(GTF):
         self.create_index(self.table, "TIdx", columns=["transcript"])
 
     def ingest(self, gtf):
+        print "Ingesting: {}".format(gtf)
+        if (gtf.endswith(".gz")):
+            with gzip.open(gtf, "rb") as gtf_file:
+                self.ingest_stream(gtf_file)
+        else:
+            with open(gtf, "rb") as gtf_file:
+                self.ingest_stream(gtf_file)
+
+    def ingest_stream(self, gtf_stream):
         BATCH_SIZE = 1000
-        with open(gtf, "rb") as gtf_file:
-            print "Ingesting: {}".format(gtf)
-            l = 0
-            n = 0
-            m = 0
-            t0 = time.time()
-            list_of_values = []
-            for line in gtf_file:
-                l += 1
-                if (line[0] == '#'):
-                    continue
-                data = line.split('\t')
-                c = data[0].strip()
-                if (len(c)>4):
-                    continue
-                s = data[1].strip()
-                f = data[2].strip()
-                p1 = int(data[3].strip())
-                p2 = int(data[4].strip())
-                score = data[5].strip()
-                if (score == '.'):
-                    score = None
-                strand = data[6].strip()
-                frame  = data[7].strip()
-                if (frame == '.'):
-                    frame = None
-                attr = data[8].strip()
+        l = 0
+        n = 0
+        m = 0
+        t0 = time.time()
+        list_of_values = []
+        for line in gtf_file:
+            l += 1
+            if (line[0] == '#'):
+                continue
+            data = line.split('\t')
+            c = data[0].strip()
+            if (len(c)>4):
+                continue
+            s = data[1].strip()
+            f = data[2].strip()
+            p1 = int(data[3].strip())
+            p2 = int(data[4].strip())
+            score = data[5].strip()
+            if (score == '.'):
+                score = None
+            strand = data[6].strip()
+            frame  = data[7].strip()
+            if (frame == '.'):
+                frame = None
+            attr = data[8].strip()
 
-                attr_data = attr.split(';')
-                gene = None
-                biotype = None
-                exon = None
-                transcript = None
-                for a in attr_data:
-                    t = a.strip().split(' ')
-                    key = t[0]
-                    if (len(t) > 1):
-                        v = t[1].strip().strip('"')
-                    else:
-                        v = None
-                        continue
-                    if (key == "gene_name"):
-                        gene = v
-                    elif (key == "exon_number"):
-                        exon = int(v)
-                    elif (key == "gene_biotype"):
-                        biotype = v
-                    elif (key == "transcript_id"):
-                        transcript = v
-                list_of_values.append((c, s, f, p1, p2, score, strand, frame, attr, gene, biotype, exon, transcript))
+            attr_data = attr.split(';')
+            gene = None
+            biotype = None
+            exon = None
+            transcript = None
+            for a in attr_data:
+                t = a.strip().split(' ')
+                key = t[0]
+                if (len(t) > 1):
+                    v = t[1].strip().strip('"')
+                else:
+                    v = None
+                    continue
+                if (key == "gene_name"):
+                    gene = v
+                elif (key == "exon_number"):
+                    exon = int(v)
+                elif (key == "gene_biotype"):
+                    biotype = v
+                elif (key == "transcript_id"):
+                    transcript = v
+            list_of_values.append((c, s, f, p1, p2, score, strand, frame, attr, gene, biotype, exon, transcript))
 
-                if (l%BATCH_SIZE == 0):
-                    try:
-                        m += self.execute_insert(self.insert_sql, list_of_values)
-                    except:
-                        #print "ERROR: line #{}: {}".format(l, line)
-                        raise
-                    del list_of_values[:]
-                    if (l % 50000 == 0):
-                        t = time.time() - t0
-                        print "{} ==> {} [{}]. Time: {}; Rate: {}".format(l, n, m, t, int(m/t))
+            if (l%BATCH_SIZE == 0):
+                try:
+                    m += self.execute_insert(self.insert_sql, list_of_values)
+                except:
+                    #print "ERROR: line #{}: {}".format(l, line)
+                    raise
+                del list_of_values[:]
+                if (l % 50000 == 0):
+                    t = time.time() - t0
+                    print "{} ==> {} [{}]. Time: {}; Rate: {}".format(l, n, m, t, int(m/t))
 
     def execute_insert(self, sql, list_of_values):
         rowcount = 0
@@ -127,7 +134,7 @@ class GTF_Import(GTF):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Import VCF files into gnomAD database")
     parser.add_argument("--files", "-f", "--input", "-i", help="Path to files to import",
-                        default=["/Users/misha/Downloads/Homo_sapiens.GRCh37.87.gtf"],
+                        default=["/db/data/misc/Homo_sapiens.GRCh37.87.gtf.gz"],
                         required=False, nargs='+')
     parser.add_argument("--dbms", help="DBMS type to import into", default="MySQL")
     parser.add_argument("--database", help="Database/Namespace", default="ensembl")
