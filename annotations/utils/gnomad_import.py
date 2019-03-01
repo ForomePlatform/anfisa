3100bacbefd887a7a6fad68de9484d08c080064e
mmcentre <michael.bouzinier@kvitneset.com>
2019-03-01 01:07:33 -0500

    MAB006             Anfisa v.0.2 (Cleaning up gnomAD)

--- annotations/utils/gnomad_import.py
+++ annotations/utils/gnomad_import.py
@@ -61,6 +61,7 @@ class GnomAD_Import(GnomAD):
                     continue
                 column = "{}_{}".format(field, ancestry)
                 self.info_columns[column] = fields[field]
+        self.info_columns["Hom"] = fields["Hom"]
 
         columns = self.vcf_columns.copy()
         columns.update(self.aux_columns)
