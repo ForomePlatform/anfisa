f4cb5b033708cf92089564ad24771707c15aa593
mmcentre <michael.bouzinier@kvitneset.com>
2019-04-05 00:29:43 -0400

    MAB008         Anfisa version 3.0 (Decision trees)

--- app/xl/xl_conf.py
+++ app/xl/xl_conf.py
@@ -2,28 +2,23 @@ from app.model.condition import ConditionMaker
 from .decision import DecisionTree
 #===============================================
 def defineDefaultDecisionTree(cond_env):
+    #return defineHearingLossTree(cond_env)
+    return defineBGMTree(cond_env)
+
+
+def defineHearingLossTree(cond_env):
     dtree = DecisionTree(cond_env)
 
-    dtree.addComment(
-        "0.     Quality check")
-    dtree.addCondition(
-        ConditionMaker.condNum("Proband_GQ", [None, 19]),
-        decision = False)
-    dtree.addCondition(
-        ConditionMaker.condNum("FS", [31, None]),
-        decision = False)
-    dtree.addCondition(
-        ConditionMaker.condNum("QD", [None, 3]),
-        decision = False)
+    addQuality(dtree)
 
     dtree.addComment(
-        '1.	    Present in HGMD as "DM"')
+        '1.	    Include if present in HGMD as "DM"')
     dtree.addCondition(
         ConditionMaker.condEnum("HGMD_Tags", ["DM"]),
         decision = True)
 
     dtree.addComment(
-        '2.	    AF < 5% AND +/- bases from intronic/exonic border')
+        '2.	    Exclude unless AF < 5% AND +/- bases from intronic/exonic border')
     dtree.addCondition(
         ConditionMaker.condNum("gnomAD_AF", [0.05, None]),
         decision = False)
@@ -33,7 +28,7 @@ def defineDefaultDecisionTree(cond_env):
         decision = False)
 
     dtree.addComment(
-        '2.a.	Present in ClinVar Path, Likely Path, VUS (worst annotation).')
+        '2.a.	Include if present in ClinVar Path, Likely Path, VUS (worst annotation), unless annotated benign by trusted submitter')
     dtree.addCondition(["and",
         ConditionMaker.condEnum("Clinvar_Benign", ["True"]),
         ConditionMaker.condEnum("Clinvar_Trusted_Benign",
@@ -41,13 +36,13 @@ def defineDefaultDecisionTree(cond_env):
         decision = True)
 
     dtree.addComment(
-        '2.b.	All de novo variants')
+        '2.b.	Include all de-novo variants')
     dtree.addCondition(
         ConditionMaker.condEnum("Callers", ["BGM_BAYES_DE_NOVO"]),
         decision = True)
 
     dtree.addComment(
-        '2.c.	All potential LOF variants '
+        '2.c.	Include all potential LOF variants '
         '(stop-codon, frameshift, canonical splice site).')
 
     dtree.addCondition(
@@ -55,16 +50,16 @@ def defineDefaultDecisionTree(cond_env):
         decision = True)
 
     dtree.addComment(
-        '3.a.	annotated as "Missense", "synonymous" '
+        '3.a.	For the rest: exclude "Missense", "synonymous" '
         'and "splice region" variants')
     dtree.addCondition(
         ConditionMaker.condNum("Severity", [None, 0]),
         decision = False)
 
     dtree.addComment(
-        '3.	AF < 0.0007 (GnomAD Overall) AND +/- 5 bases')
+        '3.	Include: AF < 0.0007 (GnomAD Overall) AND +/- 5 bases')
     dtree.addComment(
-        'PopMax < 0.01 (minimum 2000 alleles total in ancestral group)')
+        'And: PopMax < 0.01 (minimum 2000 alleles total in ancestral group)')
     dtree.addCondition(["and",
         ConditionMaker.condNum("gnomAD_AF", [None, 0.0007]),
         ConditionMaker.condNum("gnomAD_PopMax_AN", [2001, None]),
@@ -73,3 +68,38 @@ def defineDefaultDecisionTree(cond_env):
     dtree.setFinalDecision(False)
 
     return dtree
+
+
+def defineBGMTree(cond_env):
+    dtree = DecisionTree(cond_env)
+    addQuality(dtree)
+
+    dtree.addComment("Exclude common variants")
+    dtree.addCondition(
+        ConditionMaker.condNum("gnomAD_AF", [0.001, None]),
+        decision = False
+    )
+
+    dtree.addComment("Exclude low impact variants")
+    dtree.addCondition(
+        ConditionMaker.condNum("Severity", [None, 0]),
+        decision = False
+    )
+
+    dtree.setFinalDecision(True)
+
+    return dtree
+
+
+def addQuality(dtree):
+    dtree.addComment(
+        "0.     Check sequencing quality")
+    dtree.addCondition(
+        ConditionMaker.condNum("Proband_GQ", [None, 19]),
+        decision = False)
+    dtree.addCondition(
+        ConditionMaker.condNum("FS", [31, None]),
+        decision = False)
+    dtree.addCondition(
+        ConditionMaker.condNum("QD", [None, 3]),
+        decision = False)
