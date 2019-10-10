823ca0a27f77b44622b83748722f770c9b8e95a2
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2019-10-10 23:30:19 +0200

    Update PGP3140.json download link

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ mkdir -p $PROJECT_WORKDIR/data $PROJECT_WORKDIR/export/work $PROJECT_WORKDIR/log
 
 cd $PROJECT_WORKDIR/data
 echo "Loading Sample Dataset"
-curl -O -L https://www.dropbox.com/s/8u4kwp5m3px6tcz/PGP3140.json.gz
+curl -O -L https://www.dropbox.com/s/csmx5mxcy6mb875/PGP3140.json.gz
 
 echo "python -m app.storage -c $PROJECT_HOME/anfisa.json -m create -f -k ws -s PGP3140.json.gz PGP3140"
 python -m app.storage -c $PROJECT_HOME/anfisa.json -m create -f -k ws -s PGP3140.json.gz PGP3140
