3807f3d7224df9c2dd9a15e6f90c77914bd9b768
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2019-10-23 20:59:57 +0200

    Fix setup script in order to use separate docker app config

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ cd $PROJECT_WORKDIR/data
 echo "Loading Sample Dataset"
 curl -O -L https://www.dropbox.com/s/csmx5mxcy6mb875/PGP3140.json.gz
 
-echo "python -m app.storage -c $PROJECT_HOME/anfisa.json -m create -f -k ws -s PGP3140.json.gz PGP3140"
-python -m app.storage -c $PROJECT_HOME/anfisa.json -m create -f -k ws -s PGP3140.json.gz PGP3140
+echo "python -m app.storage -c $PROJECT_HOME/anfisa-docker.json -m create -f -k ws -s PGP3140.json.gz PGP3140"
+python -m app.storage -c $PROJECT_HOME/anfisa-docker.json -m create -f -k ws -s PGP3140.json.gz PGP3140
