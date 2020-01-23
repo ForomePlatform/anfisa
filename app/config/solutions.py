b9a22c8c62d4e7e1b046f8a2a589ac2b68e4caf1
mmcentre <michael.bouzinier@kvitneset.com>
2020-01-22 23:13:03 -0500

    MAB    BGM_Strict Tree

--- app/config/solutions.py
+++ app/config/solutions.py
@@ -229,6 +229,9 @@ def readySolutions():
     base_pack.regDTree("BGM xBrowse Alt",
         cfgPathSeq(["bgm_xbrowse.pyt"]),
         requires = {"trio_base"})
+    base_pack.regDTree("BGM_Strict",
+        cfgPathSeq(["bgm_strict.pyt"]),
+        requires = {"trio_base"})
     base_pack.regDTree("Trio Candidates",
         cfgPathSeq(["quality.pyt", "rare.pyt", "trio.pyt"]),
         requires = {"trio_base"})
