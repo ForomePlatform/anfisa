01d3415bcf355e86d24ccd1970d8e6c211617b0d
Vladimir A. Ulitin <v.ulitin@mobin.me>
2020-02-15 13:40:59 +0300

    favor

--- app/config/favor/f_flt.py
+++ app/config/favor/f_flt.py
@@ -21,95 +21,104 @@ import sys
 
 from app.prepare.prep_filters import FilterPrepareSetH
 
-#===============================================
+
+# ===============================================
 def _conv_len(arr):
-    if arr:
-        return len(arr)
-    return 0
+	if arr:
+		return len(arr)
+	return 0
+
 
 # ===============================================
 def defFavorFlt(metadata_record):
-    assert metadata_record.get("data_schema") == "FAVOR"
+	assert metadata_record.get("data_schema") == "FAVOR"
 
-    filters = FilterPrepareSetH(metadata_record)
+	filters = FilterPrepareSetH(metadata_record)
 
-    with filters.viewGroup("Variant"):
-        filters.statusUnit("Variant_Type", "/_filters/type",
-                           default_value="Complex",
-                           tooltip="Type of variant")
+	with filters.viewGroup("Variant"):
+		filters.statusUnit("Variant_Type", "/_filters/type",
+			default_value = "Complex",
+			tooltip = "Type of variant")
 
-    with filters.viewGroup("Coordinates"):
-        filters.statusUnit("Chromosome", "/_filters/chromosome",
-            variants=["chr1", "chr2", "chr3", "chr4", "chr5",
-                "chr6", "chr7", "chr8", "chr9", "chr10",
-                "chr11", "chr12", "chr13", "chr14", "chr15",
-                "chr16", "chr17", "chr18", "chr19", "chr20",
-                "chr21", "chr22", "chr23", "chrX", "chrY", "undefined"],
-            default_value="undefined")
+	with filters.viewGroup("Coordinates"):
+		filters.statusUnit("Chromosome", "/_filters/chromosome",
+			variants = ["chr1", "chr2", "chr3", "chr4", "chr5",
+				"chr6", "chr7", "chr8", "chr9", "chr10",
+				"chr11", "chr12", "chr13", "chr14", "chr15",
+				"chr16", "chr17", "chr18", "chr19", "chr20",
+				"chr21", "chr22", "chr23", "chrX", "chrY", "undefined"],
+			default_value = "undefined")
 
-        filters.intValueUnit("Start_Pos", "/_filters/start",
-            title="Start Position",
-            render_mode="neighborhood", default_value=sys.maxsize)
-        filters.intValueUnit("End_Pos", "/_filters/end",
-            title="End Position", default_value=0,
-            render_mode="neighborhood")
+		filters.intValueUnit("Start_Pos", "/_filters/start",
+			title = "Start Position",
+			render_mode = "neighborhood", default_value = sys.maxsize)
+		filters.intValueUnit("End_Pos", "/_filters/end",
+			title = "End Position", default_value = 0,
+			render_mode = "neighborhood")
 
-    with filters.viewGroup("Genes"):
-        genes_unit = filters.multiStatusUnit("Symbol", "/_filters/genes[]",
-            compact_mode = True)
-        filters.panelsUnit("Panels", genes_unit[0],
-            view_path="/_view/general/gene_panels")
-        filters.intValueUnit("Num_Genes", "/_view/general/genes",
-            title = "Number of overlapping genes",
-            conversion = _conv_len, default_value = 0)
+	with filters.viewGroup("Genes"):
+		genes_unit = filters.multiStatusUnit("Symbol",
+			"/_filters/genes[]",
+			compact_mode = True)
+		filters.panelsUnit("Panels", genes_unit,
+			view_path = "/_view/general/gene_panels")
+		filters.intValueUnit("Num_Genes", "/_view/general/genes",
+			title = "Number of overlapping genes",
+			conversion = _conv_len, default_value = 0)
 
-    with filters.viewGroup("gnomAD"):
-        filters.floatValueUnit("gnomAD_Total_AF",
-            "/_filters/gnomad_total_af",
-            diap = (0., 1.), default_value = 0.,
-            title = "gnomAD Allele Frequency",
-            render_mode = "log,<")
+	with filters.viewGroup("gnomAD"):
+		filters.floatValueUnit("gnomAD_Total_AF",
+			"/_filters/gnomad_total_af",
+			diap = (0., 1.), default_value = 0.,
+			title = "gnomAD Allele Frequency",
+			render_mode = "log,<")
 
-    with filters.viewGroup("GENCODE"):
-        filters.multiStatusUnit("GENCODE Category",
-            "/_filters/gencode_category",
-            compact_mode=True)
-        filters.multiStatusUnit("GENCODE Exonic Category",
-            "/_filters/gencode_exonic_category",
-            compact_mode=True)
+	with filters.viewGroup("GENCODE"):
+		filters.multiStatusUnit("GENCODE Category",
+			"/_filters/gencode_category",
+			compact_mode = True)
+		filters.multiStatusUnit("GENCODE Exonic Category",
+			"/_filters/gencode_exonic_category",
+			compact_mode = True)
 
-    with filters.viewGroup("Predictions"):
-        filters.multiStatusUnit("Clinvar",
-            "/_filters/clinvar[]",
-            default_value = "N/A",
-            title = "Clinvar")
+	with filters.viewGroup("Predictions"):
+		filters.multiStatusUnit("Clinvar",
+			"/_filters/clinvar[]",
+			default_value = "N/A",
+			title = "Clinvar")
+		filters.statusUnit("HGMD_Benign", "/_filters/hgmd_benign",
+			title = "Categorized Benign in HGMD",
+			default_value = "Not in HGMD",
+			value_map = {"True": "Benign", "False": "VUS or Pathogenic"})
+		filters.multiStatusUnit("HGMD_Tags", "/_filters/hgmd_tags[]",
+			default_value = "None")
 
-    with filters.viewGroup("Protein Function"):
-        filters.multiStatusUnit("Polyphen_2_HVAR",
-            "/_filters/polyphen2_hvar",
-            default_value = "N/A",
-            title = "Polyphen",
-            tooltip = "HumVar (HVAR) is PolyPhen-2 classifier "
-                "trained on known human variation (disease mutations vs."
-                " common neutral variants)")
-        filters.multiStatusUnit("Polyphen_2_HDIV",
-            "/_filters/polyphen2_hdiv",
-            default_value = "N/A",
-            title = "Polyphen HDIV (High sensitivity)",
-            tooltip = "HumDiv (HDIV) classifier is trained on a smaller "
-                "number of select extreme effect disease mutations vs. "
-                "divergence with close homologs (e.g. primates), which is "
-                "supposed to consist of mostly neutral mutations.")
+	with filters.viewGroup("Protein Function"):
+		filters.multiStatusUnit("Polyphen_2_HVAR",
+			"/_filters/polyphen2_hvar",
+			default_value = "N/A",
+			title = "Polyphen",
+			tooltip = "HumVar (HVAR) is PolyPhen-2 classifier "
+			          "trained on known human variation (disease mutations vs."
+			          " common neutral variants)")
+		filters.multiStatusUnit("Polyphen_2_HDIV",
+			"/_filters/polyphen2_hdiv",
+			default_value = "N/A",
+			title = "Polyphen HDIV (High sensitivity)",
+			tooltip = "HumDiv (HDIV) classifier is trained on a smaller "
+			          "number of select extreme effect disease mutations vs. "
+			          "divergence with close homologs (e.g. primates), which is "
+			          "supposed to consist of mostly neutral mutations.")
 
-        filters.multiStatusUnit("PolyPhenCat",
-            "/_filters/polyphen_cat",
-            default_value = "N/A",
-            title = "PolyPhenCat")
+		filters.multiStatusUnit("PolyPhenCat",
+			"/_filters/polyphen_cat",
+			default_value = "N/A",
+			title = "PolyPhenCat")
 
-        filters.multiStatusUnit("SIFTcat", "/_filters/sift_cat",
-            default_value = "N/A",
-            tooltip = "Sort intolerated from tolerated (An amino acid at a "
-                "position is tolerated | The most frequentest amino acid "
-                "being tolerated).")
+		filters.multiStatusUnit("SIFTcat", "/_filters/sift_cat",
+			default_value = "N/A",
+			tooltip = "Sort intolerated from tolerated (An amino acid at a "
+			          "position is tolerated | The most frequentest amino acid "
+			          "being tolerated).")
 
-    return filters
+	return filters
--- app/config/favor/f_tune.py
+++ app/config/favor/f_tune.py
@@ -18,16 +18,79 @@
 #  limitations under the License.
 #
 
-#===============================================
+from app.view.attr import AttrH
+
+
+# ===============================================
 def tuneFavorAspects(ds_h, aspects):
-    assert ds_h.getDataSchema() == "FAVOR"
-    pass
+	assert ds_h.getDataSchema() == "FAVOR"
 
-#===============================================
+	view_bas = aspects["view_bas"]
+
+	_resetupAttr(view_bas, HGMD_PMID_AttrH(view_bas))
+	pass
+
+
+# ===============================================
 def tuneFavorUnits(ds_h):
-    assert ds_h.getDataSchema() == "FAVOR"
-    pass
+	assert ds_h.getDataSchema() == "FAVOR"
+	pass
 
-#===============================================
+
+# ===============================================
 def completeFavorDS(ds_h):
-    pass
+	pass
+
+
+# ===============================================
+def _resetupAttr(aspect_h, attr_h):
+	assert attr_h.getName().lower() != attr_h.getName(), (
+			"Attribute " + attr_h.getName()
+			+ ": attributes for tuning resetup must contain uppercase letters")
+	idx1 = aspect_h.find(attr_h.getName().lower())
+	idx2 = aspect_h.find(attr_h.getName())
+	if idx1 >= 0:
+		aspect_h.delAttr(aspect_h[idx1])
+	if idx2 >= 0:
+		aspect_h.delAttr(aspect_h[idx2])
+	aspect_h.addAttr(attr_h, min(idx1, idx2)
+	if min(idx1, idx2) >= 0 else max(idx1, idx2))
+
+
+# ===============================================
+class PMID_AttrH(AttrH):
+
+	@staticmethod
+	def makeLink(pmid):
+		return ("https://www.ncbi.nlm.nih.gov/pubmed/%s" % pmid)
+
+	def __init__(self, view):
+		AttrH.__init__(self, "REFERENCES",
+			title = "Found in PubMed", tooltip = "PubMed Abstracts")
+		self.setAspect(view)
+
+	def get_pmids(self, v_context):
+		return v_context["data"]["_view"]["hgmd_pmids"]
+
+	def htmlRepr(self, obj, v_context):
+		pmids = self.get_pmids(v_context)
+		if (not pmids):
+			return None
+		links = []
+		for pmid in pmids:
+			url = self.makeLink(pmid)
+			links.append(('<span title="PubMed abstracts for %s">' % pmid)
+			             + ('<a href="%s" target="PubMed">%s</a>' % (url, pmid))
+			             + '</span>')
+		return (', '.join(links), "norm")
+
+
+# ===============================================
+class HGMD_PMID_AttrH(PMID_AttrH):
+	def __init__(self, view):
+		AttrH.__init__(self, "HGMD_PMIDs",
+			title = "HGMD PMIDs", tooltip = "PubMed Abstracts (from HGMD)")
+		self.setAspect(view)
+
+	def get_pmids(self, v_context):
+		return v_context["data"]["_view"]["general"]["hgmd_pmids"]
