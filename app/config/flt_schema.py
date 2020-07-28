d37c10a451c9ee8718d257cb3113a942b09d0f93
mmcentre <michael.bouzinier@kvitneset.com>
2020-07-28 18:10:09 -0400

    MAB    Regions and distance to boundaries are multivalue variants

--- app/config/flt_schema.py
+++ app/config/flt_schema.py
@@ -233,9 +233,9 @@ def defineFilterSchema(metadata_record):
             "/_filters/dist_from_exon_worst",
             title = "Distance From Intron/Exon Boundary (Canonical)",
             default_value = 0, render_mode = "log,<", conversion="min")
-        filters.statusUnit("Region_Canonical", "/__data/region_canonical",
+        filters.multiStatusUnit("Region_Canonical", "/__data/region_canonical[]",
             title = "Region (Canonical)", default_value = "Other")
-        filters.statusUnit("Region_Worst", "/__data/region_worst",
+        filters.multiStatusUnit("Region_Worst", "/__data/region_worst[]",
             title = "Region (Canonical)", default_value = "Other")
 
     with filters.viewGroup("gnomAD"):
