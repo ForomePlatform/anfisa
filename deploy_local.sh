12b007a322cd18ffaa6f92ddfc10189f00ac2813
mmcentre <michael.bouzinier@kvitneset.com>
2020-12-01 22:57:32 -0500

    MAB    Download URL for forome tools

--- deploy_local.sh
+++ deploy_local.sh
@@ -60,6 +60,7 @@ pip3 install -r ${repo}/requirements.txt
 forome_tools_version=0.1.6
 wheel=forome_tools-${forome_tools_version}-py3-none-any.whl
 if [ ! -f ${wheel}.gz ] ; then
+  echo curl -O -L https://github.com/ForomePlatform/forome_misc_tools/releases/download/v.${forome_tools_version}-py3/${wheel}.gz
   curl -O -L https://github.com/ForomePlatform/forome_misc_tools/releases/download/v.${forome_tools_version}-py3/${wheel}.gz
   gunzip ${wheel}.gz
   pip3 install ${wheel}
