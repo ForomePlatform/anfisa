8bef8e4e7fb64261fc731ce021eab84c9df2bff2
mmcentre <michael.bouzinier@kvitneset.com>
2021-10-05 20:28:12 -0400

    MAB merge changes from https://github.com/ForomePlatform/anfisa/commit/ab8f991b3b748b113f850fe12ec57655d09f8477

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-export COUNT_FILES=$(ls -A /anfisa/a-setup/ | wc -l)
-if [ ${COUNT_FILES} -le 2 ] ; then
-    mkdir -p /anfisa/a-setup/{data,logs,vault,export/work,ui}
+for d in /anfisa/a-setup/{data,logs,vault,export/work,ui} ; do
+    [ ! -d $d ] && mkdir -p $d
+done
+if [ ! -f /anfisa/a-setup/export/SEQaBOO_output_template_20190317.xlsx ]  ; then
+  pushd /anfisa/a-setup/export
+  curl -L -O https://www.dropbox.com/s/4dvunn3dusqc636/SEQaBOO_output_template_20190317.xlsx
+  popd
 fi
 
 if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
