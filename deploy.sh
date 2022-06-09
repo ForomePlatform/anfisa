0ae27a4e673b0096eebaaca150222c6ad8fe3638
mbychkovskiy <mikhail.bychkovskiy@quantori.com>
2022-06-09 11:06:16 +0400

    Add gene_db.js initialisation to deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -60,12 +60,20 @@ if [ ! -z "$ASETUP" ] && [ ! -z "$DRUID" ] ; then
     unzip pgp3140_wgs_hlpanel.zip -d ./pgp3140_wgs_hlpanel
   fi
 
+  pushd $ASETUP/data || exit
+  if [ ! -f gene_db.js ] ; then
+    curl -L -O https://forome-dataset-public.s3.us-south.cloud-object-storage.appdomain.cloud/gene_db.zip
+    unzip gene_db.zip
+  fi
+
   cd $WORKDIR || exit
   sed "s#ASETUP_PATH#${ASETUP}#g" setup/docker-compose.yml.template | sed "s#DRUID_WORK#${DRUID}#g" -  > docker-compose.yml
 
   docker-compose build
   docker-compose up -d
   docker ps
+
+  docker exec -it anfisa7 sh -c 'PYTHONPATH=/anfisa/anfisa/ python3 -m app.adm_mongo -c /anfisa/anfisa.json -m GeneDb /anfisa/a-setup/data/gene_db.js'
 
   docker exec -it anfisa7 sh -c 'PYTHONPATH=/anfisa/anfisa/ python3 -u -m app.storage -c /anfisa/anfisa.json -m create --reportlines 200 -f -k ws -i /anfisa/a-setup/data/examples/pgp3140_wgs_hlpanel/pgp3140_wgs_hlpanel.cfg PGP3140_HL_GENES'
   docker exec -it anfisa7 sh -c 'PYTHONPATH=/anfisa/anfisa/ python3 -u -m app.storage -c /anfisa/anfisa.json -m create --reportlines 200 -f -k xl -i /anfisa/a-setup/data/examples/pgp3140_wgs_hlpanel/pgp3140_wgs_hlpanel.cfg XL_PGP3140_HL_GENES'
