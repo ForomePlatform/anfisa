5ac401f9afd48a8a99ddb2a947a6dad6b7aa747f
nikita-kapishev <nikita.kapishev@quantori.com>
2022-07-14 13:45:09 +0500

    split entrypoint scripts and verbose Dockerfile COPY

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-for d in {/anfisa/a-setup/{data,logs,vault,export/work,ui},/data} ; do
-    [ ! -d $d ] && mkdir -p $d
-done
+#!/bin/sh
+# vim:sw=4:ts=4:et
 
-if [ ! -f /anfisa/a-setup/export/SEQaBOO_output_template_20190317.xlsx ]  ; then
-  pushd /anfisa/a-setup/export
-  curl -L -O https://www.dropbox.com/s/4dvunn3dusqc636/SEQaBOO_output_template_20190317.xlsx
-  popd
-fi
-
-if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
-then
-    sed "s#anfisa7-coordinator#${ANFISA_COORD_HOST}#g" /anfisa/anfisa.json | sed "s#anfisa7-router#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa7-mongo#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
-fi
+set -e
 
 echo "$0: Looking for shell scripts in /entrypoint.d/"
 find "/entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
