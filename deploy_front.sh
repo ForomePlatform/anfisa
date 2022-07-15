6677a9cc6a2ccde6337ccdf16cefdaeaeb69684b
nikita-kapishev <nikita.kapishev@quantori.com>
2022-07-15 15:35:16 +0500

    implement anfisa.json subst

--- deploy_front.sh
+++ deploy_front.sh
@@ -18,33 +18,33 @@ if [[ -n "$result" ]]; then
     sudo docker rmi -f anfisa-react-client
 fi
 echo "build the new docker image"
-cat << \EOF > default.nginx
-server {
-    listen 80;
+# cat << \EOF > default.nginx
+# server {
+#     listen 80;
 
-    location /app {
-        rewrite ^/(.*)$ /anfisa/$1 break;
-        proxy_pass http://anfisa7;
-        error_log /var/log/nginx/anfisa_err.log warn;
-        access_log /var/log/nginx/anfisa.log;
+#     location /app {
+#         rewrite ^/(.*)$ /anfisa/$1 break;
+#         proxy_pass http://anfisa7;
+#         error_log /var/log/nginx/anfisa_err.log warn;
+#         access_log /var/log/nginx/anfisa.log;
 
-    }
+#     }
 
-    location /anfisa {
-        rewrite ^/(.*)$ /$1/app break;
-        proxy_pass http://anfisa7;
-        error_log /var/log/nginx/anfisa_err.log warn;
-        access_log /var/log/nginx/anfisa.log;
-    }
+#     location /anfisa {
+#         rewrite ^/(.*)$ /$1/app break;
+#         proxy_pass http://anfisa7;
+#         error_log /var/log/nginx/anfisa_err.log warn;
+#         access_log /var/log/nginx/anfisa.log;
+#     }
 
-    location / {
-        client_max_body_size 100M;
-        proxy_buffering off;
-        proxy_read_timeout 3000;
-        root /usr/share/nginx/html/anfisa/;
-        try_files $uri $uri/ /index.html;
-   }
-}
+#     location / {
+#         client_max_body_size 100M;
+#         proxy_buffering off;
+#         proxy_read_timeout 3000;
+#         root /usr/share/nginx/html/anfisa/;
+#         try_files $uri $uri/ /index.html;
+#    }
+# }
 EOF
 sudo docker build -t anfisa-react-client . >> ${DIR}/output
 echo "built docker images and proceeding to delete existing container"
@@ -57,7 +57,7 @@ else
     echo "No such running container"
 fi
 echo "Deploying the updated container"
-sudo docker run --name anfisa-react-client -d -p 3000:80 --network anfisa_default anfisa-react-client
+sudo docker run --name anfisa-react-client -d -p 3000:80 -e "BACKEND=http://anfisa7" --network anfisa_default anfisa-react-client
 touch env-config.js
 cat << EOF > ./env-config.js
 window._env_ = {
@@ -67,6 +67,6 @@ EOF
 echo "Copy env-config.js into running container"
 sudo docker cp ./env-config.js anfisa-react-client:/usr/share/nginx/html/anfisa/
 echo "Add header Access-Control-Allow-Origin in Back-end container"
-sudo docker exec -d anfisa7 sed -i '59 i \\t\tadd_header 'Access-Control-Allow-Origin' '*' always;' /etc/nginx/sites-enabled/default
+sudo docker exec -d anfisa7 grep 'Access-Control-Allow-Origin' /etc/nginx/conf.d/default.conf || sed -i '/uwsgi_pass/a \\tadd_header 'Access-Control-Allow-Origin' '*' always;' /etc/nginx/conf.d/default.conf
 sudo docker restart anfisa7
 echo "Open URL http://localhost:3000"
