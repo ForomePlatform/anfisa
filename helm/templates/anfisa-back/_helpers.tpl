e795f725a85b921ac83a690c8ccc627df1c9655e
nikita-kapishev <nikita.kapishev@quantori.com>
2022-08-02 20:27:56 +0500

    Docker, helm and deployment script major changes
    - Implemented ability to use runtime environment varibles for config files
        - added envsubst for nginx template
        - added jq subst for anfisa.json template
    - Updated Dockerfile
        - images transferred to lightweight distro (alpine and debian-slim)
        - upgraded nginx version as in frontend container
        - added entrypoint scripts for templates substitution
    - Added anfisa-only helm chart to repo for deployment to kubernetes cluster
        - fixed helm templates for initial deployment
        - implemented ability to use built images from Azure container registry
        - implemented values variables substitution for CI/CD deployment
    - Modified .deploy.sh and deleted deploy_front.sh according to the latest changes
        - eliminated /anfisa/ in url path when deploy via docker
        - eliminated explicit usage of nginx config content in deployment script
        - implemented ability to build frontend image from different repo
    - Minor improvements and changes

---
+++ helm/templates/anfisa-back/_helpers.tpl
@@ -0,0 +1,6 @@
+{{/*
+Create the imagePullSecret
+*/}}
+{{- define "imagePullSecret" }}
+{{- printf "{\"auths\": {\"%s\": {\"auth\": \"%s\"}}}" .Values.imageCredentials.registry (printf "%s:%s" .Values.imageCredentials.username .Values.imageCredentials.password | b64enc) | b64enc }}
+{{- end }}
---
+++ setup/entrypoint.d/30-jqsubst-anfisa-json.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+ME=$(basename $0)
+
+json_subst() {
+  local anfisa_conf_template="/anfisa/anfisa.json.template"
+  local anfisa_conf="/anfisa/anfisa.json"
+  # if [[ -f "$anfisa_conf" ]]; then
+  #   echo "$ME: $anfisa_conf exists, skip substitute"
+  #   return 0
+  # fi
+  echo "$ME: Substitute json values from $anfisa_conf_template to $anfisa_conf"
+  jq '
+    ."file-path-def"."WORK" = "'"${ANFISA_WORK}"'" |
+    ."file-path-def"."SRC" = "'"${ANFISA_SRC}"'" |
+    ."html-title" = "'"${ANFISA_HTML_TITLE}"'" |
+    ."html-base" = "'"${ANFISA_HTML_APP_BASE}"'" |
+    ."druid"."vault-prefix" = "'"${ANFISA_DRUIDVAULTPREFIX}"'" |
+    ."druid"."index" = "'"${ANFISA_DRUIDINDEX}"'" |
+    ."druid"."coord" = "'"${ANFISA_DRUIDCOORD}"'" |
+    ."druid"."sql" = "'"${ANFISA_DRUIDSQL}"'" |
+    ."druid"."query" = "'"${ANFISA_DRUIDQUERY}"'" |
+    ."mongo-host" = "'"${ANFISA_MONGOHOST}"'" |
+    ."mongo-db" = "'"${ANFISA_MONGODB}"'" |
+    ."mongo-port" = '"${ANFISA_MONGOPORT}"'
+    ' \
+    "$anfisa_conf_template" > "$anfisa_conf"
+  if [[ ! -z "${ANFISA_DRUIDCOPYDIR}" ]]; then
+    jq '."druid"."copydir" = "'"${ANFISA_DRUIDCOPYDIR}"'"' anfisa.json > $$.json.tmp && mv $$.json.tmp anfisa.json
+  fi
+}
+
+json_subst
+
+exit 0
