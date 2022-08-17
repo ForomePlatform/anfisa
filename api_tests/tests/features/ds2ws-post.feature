e1a041e2f8957c8278792247376e27ac5be1100e
vadim.anisimov <vadim.anisimov@quantori.com>
2022-08-17 11:41:04 +0300

    improved positive Scenarios

--- api_tests/tests/features/ds2ws-post.feature
+++ api_tests/tests/features/ds2ws-post.feature
@@ -1,11 +1,24 @@
 @api
 Feature: Check ds2ws [POST] request
 
-    @positive
+    @progress
     Scenario: Create job to derive ws dataset
     Given xl Dataset is uploaded and processed by the system
     And unique ws Dataset name is generated
-    When ds2ws request with ds and ws parameters is send
+    When ds2ws request with <ds> and <ws> parameters is send
     Then response status should be 200 OK
-    And response body schema should be valid 
+    And response body schema should be valid
+    And job status should be "Done"
+    And derived dataset can be found in the dirinfo response
 
+    @progress
+    Scenario: Create job to derive ws dataset with code attribute
+    Given xl Dataset is uploaded and processed by the system
+    And unique ws Dataset name is generated
+    And valid Python code is constructed
+    When ds2ws request with <ds>, <ws> and <code> parameters is send
+    Then response status should be 200 OK
+    And response body schema should be valid
+    And job status should be "Done"
+    And derived dataset can be found in the dirinfo response
+    And <code> is present in dsinfo response for this dataset
--- api_tests/tests/step_defs/test_ds2ws_post_steps.py
+++ api_tests/tests/step_defs/test_ds2ws_post_steps.py
@@ -1,8 +1,8 @@
-import json
 from jsonschema import validate
 from pytest_bdd import scenarios, parsers, given, when, then
 from lib.api.ds2ws_api import Ds2ws
-from lib.interfaces.interfaces import EXTRA_STRING_TYPES, EXTRA_INT_TYPES, EXTRA_TYPES
+from lib.api.job_status_api import JobStatus
+from lib.interfaces.interfaces import EXTRA_STRING_TYPES, EXTRA_TYPES
 from tests.helpers.constructors import Constructor
 from tests.helpers.generators import Generator
 from lib.jsonschema.ds2ws_schema import ds2ws_schema
@@ -18,9 +18,24 @@ def unique_ws_name(dataset_type):
     return _unique_ws_name
 
 
-@when(parsers.cfparse('ds2ws request with ds and ws parameters is send'), target_fixture='ds2ws_response')
+@given(parsers.cfparse('{code_type:String} Python code is constructed',
+                       extra_types=EXTRA_STRING_TYPES), target_fixture='code')
+def code(code_type):
+    if code_type == 'valid':
+        return 'return False'
+    elif code_type == 'invalid':
+        return 'Invalid Python Code'
+
+
+@when(parsers.cfparse('ds2ws request with <ds> and <ws> parameters is send'), target_fixture='ds2ws_response')
 def ds2ws_response(dataset, unique_ws_name):
     parameters = Constructor.ds2ws_payload(ds=dataset, ws=unique_ws_name)
+    return Ds2ws.post(parameters)
+
+
+@when(parsers.cfparse('ds2ws request with <ds>, <ws> and <code> parameters is send'), target_fixture='ds2ws_response')
+def ds2ws_response(code, dataset, unique_ws_name):
+    parameters = Constructor.ds2ws_payload(ds=dataset, ws=unique_ws_name, code=code)
     return Ds2ws.post(parameters)
 
 
@@ -32,3 +47,23 @@ def assert_status(status, text, ds2ws_response):
 @then(parsers.cfparse('response body schema should be valid'))
 def assert_json_schema(ds2ws_response):
     validate(ds2ws_response.json(), ds2ws_schema)
+
+
+@then(parsers.cfparse('job status should be "Done"'))
+def assert_job_status(ds2ws_response):
+    print('ds2ws_response', ds2ws_response.json()['task_id'])
+    JobStatus.post(ds2ws_response.json()['task_id'])
+    assert 1
+    pass
+
+
+@then(parsers.cfparse('derived dataset can be found in the dirinfo response'))
+def assert_job_status(ds2ws_response):
+    assert 1
+    pass
+
+
+@then(parsers.cfparse('<code> is present in dsinfo response for this dataset'))
+def assert_job_status(ds2ws_response):
+    assert 1
+    pass
