0d27a2c36cc1804bbac8cb3ea24cf2aea8bc7500
vadim.anisimov <vadim.anisimov@quantori.com>
2022-08-18 19:30:08 +0300

    wip json schema

---
+++ api_tests/lib/jsonschema/dtree_stat_schema.py
@@ -0,0 +1,40 @@
+"""
+This module contains DtreeStat jsonschema
+"""
+
+dtree_stat_schema = {
+    "type": "object",
+    "properties": {
+        "total-counts": {
+            "type": "array",
+            "items": {
+                "type": "integer"
+            },
+            "minItems": 1,
+            "maxItems": 3
+        },
+        "filtered-counts": {
+            "type": "array",
+            "items": {
+                "type": "integer"
+            },
+            "minItems": 1,
+            "maxItems": 3
+        },
+        "stat-list": {
+            "type": "array",
+            "items": {
+                "type": "object"
+            }
+        },
+        "functions": {
+            "type": "array",
+            "items": {
+                "type": "object"
+            }
+        },
+        "rq_id": {
+            "type": "string"
+        }
+    }
+}
