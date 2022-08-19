dcd46cf96086493e9ded99cca624887acfdbb4b5
Viktoriya Elizaryan <velizaryan@thrivedetect.com>
2022-08-19 13:03:50 +0400

    add script

---
+++ scripts/send_report_to_slack.py
@@ -0,0 +1,52 @@
+import requests
+import json
+
+with open('cucumber-report.json') as json_file:
+    data = json.load(json_file)
+
+
+def scenario_result(elements):
+    _result = ''
+    _duration = 0
+    for element in elements:
+        _duration = _duration + element['result']['duration']
+        if element['result']['status'] == 'passed':
+            _result = 'PASSED'
+        elif element['result']['status'] == 'failed':
+            _result = 'FAILED'
+            break
+    return _result
+
+
+text = ''
+
+_passing = 0
+_failing = 0
+_count = 0
+for feature in data:
+    param = '\n*Feature*: ' + feature['name']
+    text = text + param + '\n'
+    for scenario in feature['elements']:
+        param = scenario_result(scenario['steps'])
+        text = text + '       *Scenario*: ' + scenario['name'] + ' ' + '[*' + param + '*]\n'
+        if param == 'PASSED':
+            _passing = _passing + 1
+        else:
+            _failing = _failing + 1
+_count = _failing + _passing
+print(text)
+result = \
+        '______________________________\n' \
+        'Tests: ' + _count.__str__() + '\n' \
+        'Passing: ' + _passing.__str__() + '\n' \
+        'Failing: ' + _failing.__str__() + '\n'
+
+print(result)
+
+text = text + '\n\n' + result
+
+BASE_URL = "https://hooks.slack.com/services/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"
+params = {"channel": "#forome-api-tests-reports", "username": "webhookbot", "text": text, "icon_emoji": ":ghost:"}
+
+response = requests.post(BASE_URL, json=params)
+print(response)
