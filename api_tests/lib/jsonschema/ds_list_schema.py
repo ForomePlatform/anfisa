50f5bb2d55be86063787e88202e6dd748f46880d
vadim.anisimov <vadim.anisimov@quantori.com>
2022-09-01 18:30:40 +0300

    controller + schema

---
+++ api_tests/lib/jsonschema/ds_list_schema.py
@@ -0,0 +1,15 @@
+"""
+This module contains DsList jsonschema
+"""
+
+ds2ws_schema = {
+    "type": "object",
+    "properties": {
+        "task_id": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "task_id"
+    ]
+}
