c829568060ad9a809e70146338c32c1290e17e8c
vadim.anisimov <vadim.anisimov@quantori.com>
2022-09-05 11:22:48 +0300

    WIP pos tc

--- api_tests/tests/features/dtree_set-post.feature
+++ api_tests/tests/features/dtree_set-post.feature
@@ -2,14 +2,16 @@
 Feature: Check dtree_set [POST] request
 
     @progress
-    Scenario: Set empty dtree for any xl dataset by code
+    Scenario Outline: Set empty dtree for any xl dataset by code
     Given "xl Dataset" is uploaded and processed by the system
-    When dtree_set request with correct "ds" and "code" parameters is send
+    When dtree_set request with correct "<ds>" and "<code>" parameters is send
     Then response status should be "200" OK
     #And response body schema should be valid by "dtree_set_schema"
     #And response body "point" dtree_point_descriptor schemas should be valid
     #And response body "dtree-list" solution_entry schemas should be valid
     #And response body "cond-atoms" condition_descriptor schemas should be valid
 
-
+    Examples:
+    | ds         | code                   |
+    | xl Dataset | generated complex code |
 
--- api_tests/tests/step_defs/test_dtree_set_post_steps.py
+++ api_tests/tests/step_defs/test_dtree_set_post_steps.py
@@ -2,13 +2,17 @@ import pytest
 from pytest_bdd import scenarios, parsers, when
 
 from lib.api.dtree_set_api import DtreeSet
+from lib.interfaces.interfaces import EXTRA_STRING_TYPES
 from tests.helpers.constructors import Constructor
 
 scenarios('../features/dtree_set-post.feature')
 
 
-@when(parsers.cfparse('dtree_set request with correct "ds" and "code" parameters is send',))
-def ds2ws_response(dataset, code, unique_ds_name):
-    parameters = Constructor.dtree_set_payload(ds=dataset, ws=unique_ds_name, code=code)
+@when(parsers.cfparse('dtree_set request with correct "{ds:String}" and "{code:String}" parameters is send',
+                      extra_types=EXTRA_STRING_TYPES))
+def ds2ws_response(ds, code, dataset):
+    if ds == 'xl Dataset':
+        ds = dataset
+    parameters = Constructor.dtree_set_payload(ds=ds, code=code)
     pytest.response = DtreeSet.post(parameters)
     return pytest.response
