201f41cba219fc4759d451f4fa2d0e2ecef7cd18
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 18:23:14 +0000

    Adding tarfile member sanitization to extractall()

--- app/ws/ws_io.py
+++ app/ws/ws_io.py
@@ -97,7 +97,29 @@ def importWS(vault_h, ds_name, tar_content):
                 return {
                     "error": f"Bad dataset archive: file {fname} is required"}
         os.mkdir(ds_dir)
-        tar.extractall(path = ds_dir)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar, path=ds_dir)
 
     try:
         with open(ds_dir + "/dsinfo.json", "r", encoding = "utf-8") as inp:
